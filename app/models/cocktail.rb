9057bd8e6e01873570db7f3b4f521e997c858576
romainbazeler <romain.bazeler@hotmail.com>
2020-08-15 13:03:51 -0400

    validation

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,4 +1,11 @@
 class Cocktail < ApplicationRecord
   # has_many :doses, dependent: :destroy
-  has_many :doses
+
+  # A cocktail has many doses
+  has_many :doses 
+
+  # A cocktail has many ingredients through doses
+  has_many :ingredients, through doses
+
+  validates :name
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  # A dose must have a description, a cocktail and an ingredient, and [cocktail, ingredient] pairings should be unique.
+
+  validates :description, :cocktail_id, :ingredients_id
 end
