2e641c8bdd6b3bfee8cedfb7ef515c121c762173
romainbazeler <romain.bazeler@hotmail.com>
2020-08-15 14:07:44 -0400

    ajout d un scope dans modele

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,10 +2,10 @@ class Cocktail < ApplicationRecord
   # has_many :doses, dependent: :destroy
 
   # A cocktail has many doses
-  has_many :doses 
+  has_many :doses, dependent: :destroy
 
   # A cocktail has many ingredients through doses
-  has_many :ingredients
+  has_many :ingredients, through: :doses
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -5,5 +5,8 @@ class Dose < ApplicationRecord
   # A dose must have a description, a cocktail and an ingredient, and [cocktail, ingredient] pairings should be unique.
 
   validates :description, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient }
+
+
 end
 
